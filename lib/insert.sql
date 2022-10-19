8f1df0d48f31fbbfd1b3b5101bcaa9e0f52b4396
Noahkimtai <noahkimtai24@gmail.com>
2022-10-19 20:47:41 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(id,name,age,sex,color,temperament,alive)VALUES
+(0,'beary',45,'F','black','aggresive',0),
+(1,'choco',45,'M','black','gentle',0),
+(2,'beary',45,'F','blue','aggresive',1),
+(3,'coco',45,'M','black','abbrasive',0),
+(4,'beary',45,'F','white','aggresive',1),
+(5,null,45,'M','red','naive',1),
+(6,'beary',45,'M','red','naive',1),
+(7,'beary',45,'M','red','naive',1);
